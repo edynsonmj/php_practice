41834a51bc44a60237ce02b383706f9947f4644a
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2024-01-12 13:30:06 -0500

    correccion nombre: solucion 2

--- p88 merge sorted array/p88_merge_sorted_array_solucion2.php
+++ p88 merge sorted array/p88_merge_sorted_array_solucion2.php
@@ -1,7 +1,7 @@
 <?php
 /*
 ejercicio 88: unir 2 arreglos
-//solucion 1.
+//solucion 2.
 You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
 representing the number of elements in nums1 and nums2 respectively.
 
